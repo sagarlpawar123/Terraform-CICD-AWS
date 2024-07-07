a373cf23c0ba9317c671e333606fb0e95b0b9d96
Sagar Pawar <67407714+sagarlpawar123@users.noreply.github.com>
2024-07-07 10:56:45 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 provider "aws" {
-    region = "us-east-1"  
+    region = "ap-south-1"  
 }
 
-resource "aws_instance" "foo" {
-  ami           = "ami-05fa00d4c63e32376" # us-west-2
+resource "aws_instance" "project-tf" {
+  ami           = "ami-01376101673c89611" # ap-south-1
   instance_type = "t2.micro"
   tags = {
       Name = "TF-Instance"
